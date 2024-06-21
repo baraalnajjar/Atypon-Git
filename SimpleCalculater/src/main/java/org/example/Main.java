2f17ce1e21711447f7849c74daaaec8486081654
baraa <baraanajjarc@gmail.com>
2024-06-22 02:08:53 +0300

    add the project

---
+++ SimpleCalculater/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+
+    public static double add(double a, double b) {
+        System.out.print("addition of a and b = ");  System.out.println(a = b);
+        return a = b;
+    }
+    public static void main(String[] args)
+    {
+        int a=6;
+        int b=3;
+        add( a,  b);
+
+
+    }
+}
