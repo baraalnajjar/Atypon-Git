a3051e5fc4c245e5e880fa9559967a2583594549
baraa <baraanajjarc@gmail.com>
2024-06-22 03:29:21 +0300

    features2

--- SimpleCalculater/src/main/java/org/example/Main.java
+++ SimpleCalculater/src/main/java/org/example/Main.java
@@ -6,12 +6,18 @@ public class Main {
         System.out.print("addition of a and b = ");  System.out.println(a + b);
         return a + b;
     }
+
+    public static double sub(double a, double b) {
+        System.out.print("subtraction of a and b = ");  System.out.println(a - b);
+        return a - b;
+    }
+
     public static void main(String[] args)
     {
         int a=6;
         int b=3;
         add( a,  b);
-
+        sub( a,  b);
 
     }
 }
