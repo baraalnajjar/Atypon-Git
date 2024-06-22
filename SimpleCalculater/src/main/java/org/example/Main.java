c3b1eb392eed6891eaff1083df24dd2614bc73d3
baraa <baraanajjarc@gmail.com>
2024-06-22 03:24:40 +0300

    hotfix

--- SimpleCalculater/src/main/java/org/example/Main.java
+++ SimpleCalculater/src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 public class Main {
 
     public static double add(double a, double b) {
-        System.out.print("addition of a and b = ");  System.out.println(a = b);
-        return a = b;
+        System.out.print("addition of a and b = ");  System.out.println(a + b);
+        return a + b;
     }
     public static void main(String[] args)
     {
